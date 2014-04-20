fa83ce8971657d1193ee189b2d08d54648637c76
Pranav Waila <pranav.waila@gmail.com>
2014-04-20 20:01:43 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## function to make special matrix this is made for coursera r programming assignment
+## functions cashes the matrix
 
-## Write a short comment describing this function
+## set the value of matrix
+## get the value of matrix
+## set inverse of matrix
+## get inverse of matrix
 
 makeCacheMatrix <- function(x = matrix()) {
+
+	m <- NULL
+      set <- function(y) {
+               x <<- y
+               m <<- NULL
+      }
+      get <- function() x
+      setinverse <- function(inverse) m <<- inverse
+      getinverse <- function() m
+      list(set = set, get = get,
+      	setinverse = setinverse,
+	      getinverse = getinverse)
 
 }
 
 
-## Write a short comment describing this function
+## calculates the inverse of special mean 
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+	m <- x$getinverse()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinverse(m)
+        m
 }
